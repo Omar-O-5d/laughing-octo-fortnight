56c38c49c2e061b639887a5b570d3a50b14f2a6e
Omar-O-5d <137425794+Omar-O-5d@users.noreply.github.com>
2024-01-30 23:34:54 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+song="";
+song2="";
+scoreLeftWrist=0;
+scoreRightWrist=0;
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload() {
+    song=loadSound("music.mp3");  
+    song2=loadSound("watr.mp3")
+  }
+
+  function setup() {
+    canvas=createCanvas(600 , 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();  
+
+     poseNet = ml5.poseNet(modelLoaded)
+
+}
+
+function modelLoaded() {
+  console.log('model Loaded')
+}
+
+function draw() {
+    image(video,0,0,600,500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+  
+    }
+
+    function gotPoses( error , results) {
+      if (results>0) {
+ leftWristX=0;
+ leftWristY=0;
+ rightWristX=0;
+ rightWristY=0;
+
+      }
+    }
